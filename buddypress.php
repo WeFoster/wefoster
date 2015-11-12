0e910c6e157c81ed9d92c9b577d79bf7b82f71ef
Bowe Frankema <bowe@cfcommunity.net>
2015-11-12 10:32:51 +0100

    Add extra hooks

--- buddypress.php
+++ buddypress.php
@@ -12,16 +12,25 @@
 
 <?php get_template_part('header'); ?>
 
+   <?php do_action('before_bp_page'); ?>
+
     <div class="main <?php do_action('class_main'); ?>" role="main">
 
-      <?php do_action('before_page_header'); ?>
-      <?php get_template_part('buddypress/page', 'header'); ?>
-      <?php do_action('after_page_header'); ?>
-      <?php get_template_part('buddypress/content', 'page'); ?>
-        <?php do_action('after_page_content'); ?>
+        <?php do_action('open_bp_page_content'); ?>
+
+        <?php do_action('before_bp_page_header'); ?>
+
+        <?php get_template_part('buddypress/page', 'header'); ?>
+
+        <?php do_action('after_bp_page_header'); ?>
+
+        <?php get_template_part('buddypress/content', 'page'); ?>
+
+        <?php do_action('close_bp_page_content'); ?>
 
     </div><!-- /.main -->
 
+   <?php do_action('after_bp_page'); ?>
 
 <?php get_template_part('sidebar'); ?>
 <?php get_template_part('footer'); ?>
--- footer.php
+++ footer.php
@@ -13,6 +13,8 @@
 </div><!-- /.content -->
 </div><!-- /.wrap -->
 
+<?php do_action('before_footer'); ?>
+
   <footer class="content-info <?php do_action('class_footer'); ?>" role="contentinfo">
 
     <div class="<?php do_action('class_container'); do_action('class_container_footer'); ?>">
