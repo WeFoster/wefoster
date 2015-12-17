ff1e02f8503558359946c08c8e8b4ca1fa5426b5
Bowe Frankema <bowe@cfcommunity.net>
2015-12-17 13:37:16 +0100

    Remove dummy code

--- page.php
+++ page.php
@@ -19,10 +19,6 @@
 
         <?php get_template_part('templates/loops/content', 'page'); ?>
 
-        <!-- My Custom Content -->
-              <p>This is some custom content after my page content is shown. I'm adding this by overwriting my template via a Child Theme!</p>
-        <!-- My Custom Content -->
-
         <?php do_action('close_page_content'); ?>
     </div><!-- /.main -->
 
