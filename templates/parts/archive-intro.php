cda88247a595836f7a936fc7beba29f67f0d42d0
Bowe Frankema <bowe@cfcommunity.net>
2015-12-18 19:11:25 +0100

    Clean up archive intro

--- templates/parts/archive-intro.php
+++ templates/parts/archive-intro.php
@@ -1,66 +1,46 @@
 <?php
+//Are we on a category archive?
+if ( is_category() ):
+  $category_description = category_description();
+  if ( !empty( $category_description ) ) {
+    echo '<div class="box-intro tag-description">';
+      print $category_description;
+    echo '</div>';
+  }
+endif;
 
-// show the category box when on a category page
-    if ( is_category() ):
-?>
-        <div class="box-intro category-description">
-            <?php
-                // get cat desc
-                $category_description = category_description();
-                // print it?
-                if ( !empty( $category_description ) ) {
-                    print $category_description;
-                }
-            ?>
-        </div>
-<?php
+//Or taxonomy?
+if ( is_tax() ):
+  $tax_description = tag_description();
+  if ( ! empty( $tax_description ) ) {
+    echo '<div class="box-intro tag-description">';
+        echo  $tax_description ;
+    echo '</div>';
+  }
+endif;
+
+//Or tag?
+if ( is_tag() ):
+  //Show The tag box when on a Tags Page
+  $tag_description = term_description();
+      if ( ! empty( $tag_description ) ) {
+      echo '<div class="box-intro tag-description">';
+          echo $tag_description ;
+      echo '</div>';
+    }
+endif;
+
+//Or is this the author archive??
+if ( is_author() ):
+  // queue the first post, that way we know who the author is when we
+  // try to get their name, URL, description, avatar, etc.
+  if ( have_posts() ):
+    the_post();
+    // if a user has filled out their description, show a bio on their entries.
+    if ( get_the_author_meta( 'description' ) ):
+      get_template_part('templates/parts/author-box');
     endif;
-?>
-
-<!-- Show The tag box when on a Tags Page -->
-<?php
-    if ( is_tax() ):
-?>
-        <div class="box-intro tag-description">
-            <?php
-                    $tag_description = tag_description();
-                    if ( ! empty( $tag_description ) )
-                        echo  $tag_description ;
-                ?>
-            </div>
-<?php
-    endif;
-?>
-
-<!-- Show The tag box when on a Tags Page -->
-<?php
-    if ( is_tag() ):
-?>
-        <div class="box-intro taxonomy-description">
-            <?php
-                    $tag_description = term_description();
-                    if ( ! empty( $tag_description ) )
-                        echo  $tag_description ;
-                ?>
-            </div>
-<?php
-    endif;
-?>
-
-<?php
-    if ( is_author() ):
-        // queue the first post, that way we know who the author is when we
-        // try to get their name, URL, description, avatar, etc.
-        if ( have_posts() ):
-            the_post();
-
-            // if a user has filled out their description, show a bio on their entries.
-            if ( get_the_author_meta( 'description' ) ):
-                  get_template_part('templates/parts/author-box');
-            endif;
-
-            // reset the loop so we don't break later queries
-            rewind_posts();
-        endif;
-    endif;
-?>
+    // reset the loop so we don't break later queries
+    rewind_posts();
+  endif;
+endif;
