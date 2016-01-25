498679faf1f545a305953c72d1cbc476ddad6a39
Bowe Frankema <bowe@cfcommunity.net>
2016-01-25 14:55:57 +0100

    Wrap secondary navigation

--- templates/header/secondary-navigation.php
+++ templates/header/secondary-navigation.php
@@ -2,20 +2,22 @@
 if ( has_nav_menu( 'secondary_navigation' ) ) {
 do_action('before_secondary_header_navigation');
 ?>
-<div class="secondary-nav-wrap <?php do_action('class_container'); do_action('class_container_navbar'); ?>">
-<nav class="collapse navbar-collapse <?php do_action('class_navbar'); ?>" role="navigation">
-		 <?php do_action('open_header_navigation_secondary'); ?>
-          <?php
-              wp_nav_menu(array(
-									'theme_location' => 'secondary_navigation',
-              		'menu' => 'Secondary Navigation',
-              		'fallback_cb'    => 'secondary_navigation',
-              		'menu_class' => 'nav navbar-nav navbar-secondary'));
-          ?>
+<div class="secondary-nav-wrap container-fluid <?php do_action('class_container_secondary_navbar'); ?> box-brand-secondary">
+<div class="container">
+		<nav class="collapse navbar-collapse <?php do_action('class_navbar'); ?>" role="navigation">
+				 <?php do_action('open_header_navigation_secondary'); ?>
+		          <?php
+		              wp_nav_menu(array(
+											'theme_location' => 'secondary_navigation',
+		              		'menu' => 'Secondary Navigation',
+		              		'fallback_cb'    => 'secondary_navigation',
+		              		'menu_class' => 'nav navbar-nav navbar-secondary'));
+		          ?>
 
-          <?php do_action('close_header_navigation_secondary'); ?>
+		          <?php do_action('close_header_navigation_secondary'); ?>
 
-</nav>
+		</nav>
+</div>
 </div>
 <?php
 }
