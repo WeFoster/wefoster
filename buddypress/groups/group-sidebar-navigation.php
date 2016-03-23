bd294390daabdd71ef24c6421a0953c9108af2f0
Bowe Frankema <bowe@cfcommunity.net>
2016-03-23 10:27:44 +0100

    Small improvements

--- buddypress/groups/group-sidebar-navigation.php
+++ buddypress/groups/group-sidebar-navigation.php
@@ -1,7 +1,8 @@
-<?php do_action('before_bp_group_navigation'); ?>
+<?php do_action('before_bp_group_sidebar_navigation'); ?>
 <div class="sidebar-activity-tabs no-ajax item-list-tabs vertical-list-tabs widget" role="navigation">
 <?php do_action('open_bp_group_sidebar_navigation'); ?>
 	<ul id="object-nav" class="sidebar-nav">
+  	<?php do_action( 'before_group_options_nav' ); ?>
 		<?php bp_get_options_nav(); ?>
 		<?php do_action( 'bp_group_options_nav' ); ?>
 	</ul>
