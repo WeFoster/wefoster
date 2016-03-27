37466b9166b48aae986da01b8dcef3773e59a6a2
Bowe Frankema <bowe@cfcommunity.net>
2016-03-27 13:19:35 +0200

    Make wp_head only hook in <head>. Review #09
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- header.php
+++ header.php
@@ -18,10 +18,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php do_action( 'wf_head' ); ?>
-	<?php wp_head(); ?>
 	<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> Feed" href="<?php echo esc_url( get_feed_link() ); ?>">
-	<?php do_action( 'wf_close_head' ); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
