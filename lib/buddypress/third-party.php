c01de33e77916238a9d4dec1e19e53b43ff39f52
Bowe Frankema <bowe@cfcommunity.net>
2016-05-30 19:00:54 +0200

    Remove unneeded RTMedia Compat
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- lib/buddypress/third-party.php
+++ lib/buddypress/third-party.php
@@ -1,14 +1,1 @@
 <?php
-/**
- * Provide Support for BuddyPress Media
- * https://rtmedia.io/docs/developers/themes/theme-media-tab/
- * @since 1.0.0
- */
-if ( ! function_exists( 'rtmedia_autoloader' ) ) {
-	function wff_rtmedia_main_template_include($template, $new_rt_template){
-			global $wp_query;
-			$wp_query->is_page = true;
-			return locate_template('buddypress.php');
-	}
-	add_filter('rtmedia_main_template_include', 'wff_rtmedia_main_template_include', 20, 2);
-}
